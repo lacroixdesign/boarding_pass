4da4a076673ee7f24a9153786faa8d97b375c8a7
Michael LaCroix <michael@lacroixdesign.net>
2013-09-15 05:03:23 -0500

    update export property to match node-sass option

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var path = require('path');
 
-function buildLoadPaths() {
-  var neatPaths         = require('node-neat').loadPaths;
+function getPaths() {
+  var neatPaths         = require('node-neat').includePaths;
   var boardingPassPaths = path.join(__dirname, 'app/assets/stylesheets');
   return neatPaths.concat(boardingPassPaths);
 }
 
 module.exports = {
 
-  loadPaths: buildLoadPaths()
+  includePaths: getPaths()
 
 };
