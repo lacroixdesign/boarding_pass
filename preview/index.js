0e851d8cb3fe0a95082acc6358697ce53c969c7d
Michael LaCroix <michael@lacroixdesign.net>
2014-01-14 14:47:47 -0600

    fix Typeplate issue; fix sass compile-on-request issue

--- preview/index.js
+++ preview/index.js
@@ -1,25 +1,37 @@
 var express = require('express')
   , http    = require('http')
-  , path    = require('path')
+  , fs      = require('fs')
   , sass    = require('node-sass')
   , boardingPass = require('../index.js');
 
 var app = module.exports = express();
 
-var sassServer = sass.middleware({
-  src:   __dirname
-, dest:  __dirname + '/public'
-, debug: true
-, includePaths: boardingPass.includePaths
-});
-
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
-app.use(sassServer);
+var compileSass = function (req, res, next) {
+  var inputFile  = __dirname + '/style.scss',
+      outputFile = __dirname + '/public/style.css';
+  sass.render({
+    file: inputFile,
+    success: function (css) {
+      fs.writeFile(outputFile, css, function() {
+        log('render', outputFile);
+        next();
+      });
+    },
+    error: function (err) {
+      logError('ERROR', err);
+      next();
+    },
+    includePaths: boardingPass.includePaths
+  });
+};
+
+app.use(compileSass);
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index');
@@ -28,3 +40,11 @@ app.get('/', function (req, res) {
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Server listening on port ' + app.get('port'));
 });
+
+// loggers
+function log (key, val) {
+  console.error('  \033[90m%s :\033[0m \033[36m%s\033[0m', key, val);
+}
+function logError (key, val) {
+  console.error('  \033[31m\033[40m%s\033[0m :\033[0m \033[36m%s\033[0m', key, val);
+}
